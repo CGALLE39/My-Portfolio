a81c419119d8c8e3a89ddfc9e1554058684f8c7b
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-03-18 04:28:06 -0500

    home animation

---
+++ react-portfolio/react-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import Layout from './components/Layout';
+import Home from './components/Home';
+import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+      <>
+      <Routes>
+            <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} ></Route>
+            </Route>
+      </Routes>
+      </>
+  )
+}
+
+export default App;
