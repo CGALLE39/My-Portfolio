a76626a82e3807b1b588f1e743c50b836ffca1fc
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-03-22 18:50:25 -0500

    about

--- react-portfolio/react-portfolio/src/App.js
+++ react-portfolio/react-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import Layout from './components/Layout';
 import Home from './components/Home';
+import About from './components/Home/About';
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import './App.scss';
@@ -10,6 +11,7 @@ function App() {
       <Routes>
             <Route path="/" element={<Layout />} >
             <Route index element={<Home />} ></Route>
+             <Route path="/about" element={<About />} ></Route>
             </Route>
       </Routes>
       </>
---
+++ react-portfolio/react-portfolio/src/components/Home/About/index.js
@@ -0,0 +1,7 @@
+
+
+const About = () => 'hello'
+
+
+
+export default About;
