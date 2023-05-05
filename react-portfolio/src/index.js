f39bc42af913d6078415fbb07841c03e435f030b
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-05-05 09:14:04 -0500

    render issues corrected

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
